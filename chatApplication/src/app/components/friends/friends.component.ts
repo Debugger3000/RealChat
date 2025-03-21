e6af84dd1cfb29344e666457becb7cfd23e57c95
Breanna Moloney <144464731+Violeterror404@users.noreply.github.com>
2025-03-21 15:31:27 -0400

    Changes to friends component - added mock friends to ref styling. Added default profile pic in public folder
    Updated the friends component. Added mock friends to reference the styling; the actual users should use the same styling. Added default user image (currently used in just the mock users)
    
    Also added concept for the request stat message in the html. Brief idea for logic within the ts file.

--- chatApplication/src/app/components/friends/friends.component.ts
+++ chatApplication/src/app/components/friends/friends.component.ts
@@ -8,17 +8,24 @@ import { HomeComponent } from '../../home/home.component';
   styleUrl: './friends.component.scss'
 })
 export class FriendsComponent {
+requestSuccess: any; 
 
-
+  
   // call this to chat the state of home component
   // changeHome() {
     
   //   this.chatRoomStatePass.set('friends');
   // }
 
+  //logic idea: 
+    //start with requestSuccess as null - so the message isn't visible
+    //on either success or fail set bool to either true or false - to assign the styling & message
+    //for either success or fail have a timeout, which once done sets requestSuccess to null again (to hide the message, so it's not always visible)
+  
+
   toggleFriendRequests() {
 
-    console.log("friend requets button clicked...");
+    console.log("friend request button clicked...");
 
   }
 
