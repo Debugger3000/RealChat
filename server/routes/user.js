73a619d7884c4dc786d51e6f2ca4d896f4192d8e
Debugger3000 <145146370+Debugger3000@users.noreply.github.com>
2025-03-31 12:50:33 -0400

    er333

--- server/routes/user.js
+++ server/routes/user.js
@@ -31,11 +31,12 @@ userRoutes.get("/", async (req,res,next) => {
 
 //GET - get a specific user by id
 userRoutes.get("/me", async (req, res) => {
+    console.log("GET ME route has been hit !!!");
     
     try{
-        console.log("req user", req.user);
-        console.log("this is the req...", req);
-        console.log("this is the req cookies... ",req.cookies);
+        // console.log("req user", req.user);
+        console.log("this is the req...", req.body);
+        // console.log("this is the req cookies... ",req.cookies);
         // send back user data for themselves by grabbing their user data from 'req.user'
         res.status(200).json({user: req.user});
     } catch (error) {
